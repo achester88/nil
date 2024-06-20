7e45fec21a874277c406c7377411f5cc5d63035f
achester88 <anthonychester71@gmail.com>
2024-06-20 16:48:15 -0400

    Work on lexer for /* */ groups

--- src/nil/lexer.rs
+++ src/nil/lexer.rs
@@ -1,7 +1,17 @@
+use regex::Regex;
+
 use crate::nil::token::Token;
 
 pub fn tokenizer(input: String) -> Vec<Token> {
-    println!("{}", input);
+    let mut tokens = Vec::new();
 
-    unimplemented!();
+    let code_re = Regex::new(r"\/\*([\s\S]*?)\*\/").unwrap();
+
+    let code: Vec<String> = code_re.captures_iter(&input)
+        .filter_map(|segments| segments.as_str().parse().ok())
+        .collect();
+
+    println!("{:#?}", code);
+
+    return tokens;
 }
--- src/nil/token.rs
+++ src/nil/token.rs
@@ -1,3 +1,11 @@
 pub enum Token {
-
+    Delimiter, //; char
+    OpeningPars,
+    ClosingPars,
+    OpeningBrac,
+    ClosingBrac,
+    Comma,
+    Ident(String),
+    Number(f64),
+    Operator(String)
 }
