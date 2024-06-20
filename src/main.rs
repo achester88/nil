060497f289501885c28ab71756824a4d88dfc4d1
achester88 <anthonychester71@gmail.com>
2024-06-20 15:16:35 -0400

    Created CLI for lang and basic files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,55 @@
+use std::env;
+use std::panic;
+
+mod nil;
+use crate::nil::lexer;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    panic::set_hook(Box::new(|_info| {
+        // do nothing
+    }));
+
+    match args.len() {
+        1 => {
+            println!("\x1b[91mError\x1b[0m Missing Argument:\n <path>");
+            panic!();
+        }
+        2 => {
+            let path = &args[1];
+
+            match path.as_ref() {
+                "--help" => println!("USEAGE:\n nil <path>"),
+                _ => {
+                    let parts: &Vec<&str> = &path.as_str().split('.').collect();
+                    if parts.len() == 1 {
+                        println!("\x1b[91mError\x1b[0m Unkown Argument Passed");
+                    panic!();
+                    } else {
+                        let ent = parts[parts.len()-1];
+                        if ent != "nil" {
+                            println!("\x1b[91mError\x1b[0m File has unexpected file extension of .{}", ent);
+                            panic!();
+                        }
+
+                        let mut content: String;
+                        match std::fs::read_to_string(&path) {
+                            Ok(x) => content = x,
+                            Err(_e) => {
+                                println!("\x1b[91mError\x1b[0m Could Not Find File at Path: `{}`", path);
+                                panic!();
+                            }
+                        }
+
+                        //Start of Processing
+                        lexer::tokenizer(content);
+                    }
+                }
+            }
+        }
+        _ => {
+            println!("\x1b[91mError\x1b[0m Unkown Argument Passed");    
+        }
+    }
 }
---
+++ src/nil/lexer.rs
@@ -0,0 +1,7 @@
+use crate::nil::token::Token;
+
+pub fn tokenizer(input: String) -> Vec<Token> {
+    println!("{}", input);
+
+    unimplemented!();
+}
